70d80ed519c727f9c093944fe98a90df51b9b8c6
Sameer Hasan <simplysamy@gmail.com>
2026-01-11 15:18:10 +0530

    Add variable validation

--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
 variable "repo_count" {
   description = "Number of repositories to create"
   type        = number
-  default     = 2
+  default     = 1
+
+  validation {
+    condition = var.repo_count < 5
+    error_message = "Don't deploy more than 5 repo"
+  }
 }
+
